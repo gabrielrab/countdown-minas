c59e977d2a6e8e193bfa822a20be8fe4c44ae016
Gabriel Rabelo Camargos <rabelogabriel72@gmail.com>
2019-12-07 01:05:10 -0300

    Implement date-fns

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { formatDistance } from "date-fns";
+import pt from "date-fns/locale/pt";
+
+import "./style.css";
 
 function App() {
+  const [value, setValue] = useState();
+  const [load, setLoad] = useState();
+
+  useEffect(() => {
+    function loadDate() {
+      const res = formatDistance(
+        new Date(),
+        new Date("Fri Dec 13 2019 01:01:35"),
+        {
+          locale: pt
+        }
+      );
+      const [day, word] = res.split(" ");
+      const recive_load = 100 - day * 10;
+      setLoad(recive_load);
+      setValue(res);
+    }
+    loadDate();
+  });
+
   return (
     <>
-      <h1>Go to Minas</h1>
+      <header>
+        <h1>Tempo restante</h1>
+        <div className="progress-bar">
+          <div className="progress-value" style={{ width: `${load}%` }}></div>
+        </div>
+        <h2>{value}</h2>
+      </header>
     </>
   );
 }
